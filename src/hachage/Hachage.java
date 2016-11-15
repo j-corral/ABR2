d33b98d343fe4a8615cda6a7e289601a48a64b52
KenAdBlock <kennycg13@gmail.com>
2016-11-15 15:15:10 +0100

    Begin of Hachage class

--- src/hachage/Hachage.java
+++ src/hachage/Hachage.java
@@ -5,13 +5,22 @@ import ABR.Donnee;
 /**
  * Created by jonathan on 15/11/16.
  */
-public class Hachage<T extends Donnee> {
+public abstract class Hachage<T extends Donnee> {
 
     int m;
 
     T donnee;
 
+    public Hachage(int m) {
+
+        this.m = m;
+    } // Constructor
 
 
+    public abstract void add(T d);
+
+    public abstract boolean recherche(T d);
+
+    public abstract int h(T d);
 
 }
